da6319d97f4bfcd3186a300acf731c59fdd835d8
Ezequiel Santos <111365548+EzequiellSantos@users.noreply.github.com>
2024-04-01 09:36:29 -0300

    Sending Back-end

---
+++ liturgia-diaria/meu-backend/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+
+const app = express();
+
+app.use(cors());
+
+app.get('/', async (req, res) => {
+    try {
+        const response = await axios.get('https://api-liturgia-diaria.vercel.app/');
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch data from the API' });
+    }
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
